b1337b0cc125052b8e3590a7041b8dff1242a393
Alexander Steele <alexsteele110@gmail.com>
2017-08-31 20:40:03 -0400

    init commit

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+mongoose.model('users', userSchema);
+
+// Freely add and remove properties from userSchema here
---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); // ID assigned by mongo, not google
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+// Below is where you can add in any strategy you want, with similar format
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
+    }
+  )
+);
