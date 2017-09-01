b5c0757cf6888f14f8dce30ad1e79481eac4b881
Alexander Steele <alexsteele110@gmail.com>
2017-09-01 13:05:38 -0400

    added client

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
+const Landing = () => <h2>Landing</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
