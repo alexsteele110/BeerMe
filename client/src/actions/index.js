75df4b0ace418e63448e30baa331a273919403af
Alexander Steele <alexsteele110@gmail.com>
2017-09-14 20:58:20 -0400

    CORS issues, need to use node for API request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 import { FETCH_BEERS } from './types';
+const keys = require('../clientconfig/clientkeys');
+
+const ROOT_URL = `http://api.brewerydb.com/v2/search?key=${keys.breweryKey}`;
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
@@ -8,6 +11,12 @@ export const fetchUser = () => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const fetchBeers = () => async dispatch => {
-  const res = await axios.get();
-};
+export function fetchBeers(beer) {
+  const url = `${ROOT_URL}&type=beer&q=${beer}`;
+  const res = axios.get(url);
+
+  return {
+    type: FETCH_BEERS,
+    payload: res
+  };
+}
