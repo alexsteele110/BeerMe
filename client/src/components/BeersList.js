c4cc593aff02ea2f90b3ded802706312c4f0dbee
Alexander Steele <alexsteele110@gmail.com>
2017-11-17 12:16:06 -0500

    beginning of carded search results styling

--- client/src/components/BeersList.js
+++ client/src/components/BeersList.js
@@ -1,24 +1,80 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+  card: {
+    display: 'flex',
+    justifyContent: 'flex-start'
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '80%'
+  },
+  content: {
+    flex: '1 0 auto'
+  },
+  cover: {
+    width: 251,
+    height: 251
+  }
+});
 
 class BeersList extends Component {
+  renderContent = () => {
+    const { classes } = this.props;
+    if (this.props.beers.length === 0) {
+      return (
+        <div>
+          <h3>No content yet</h3>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <Card className={classes.card}>
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography type="headline">
+                {this.props.beers.name}
+              </Typography>
+              <Typography type="subheading" color="secondary">
+                {this.props.beers.description}
+              </Typography>
+            </CardContent>
+          </div>
+          <CardMedia
+            className={classes.cover}
+            image={this.props.beers.labels.medium}
+            title="Live from space album cover"
+          />
+        </Card>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div>
-        <h2>Beers List</h2>
-        <h4>
-          {this.props.beers.name}
-        </h4>
-        <h4>
-          {this.props.beers.description}
-        </h4>
+        {this.renderContent()}
       </div>
     );
   }
 }
 
+BeersList.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired
+};
+
 function mapStateToProps({ beers }) {
   return { beers };
 }
 
-export default connect(mapStateToProps)(BeersList);
+export default withStyles(styles, { withTheme: true })(
+  connect(mapStateToProps)(BeersList)
+);
