e6a3bfb1fc42173dab03edf4db238a220b9e2a3f
Alexander Steele <alexsteele110@gmail.com>
2017-11-22 16:09:18 -0500

    added route for info on single beer based on ID

--- routes/beerRoutes.js
+++ routes/beerRoutes.js
@@ -1,18 +1,34 @@
 const axios = require('axios');
 const keys = require('../config/keys');
 
-const ROOT_URL = `http://api.brewerydb.com/v2/search?key=${keys.breweryKey}`;
+const ROOT_URL = 'http://api.brewerydb.com/v2/';
 
 module.exports = app => {
   app.get('/api/search/:keyword', (req, res) => {
     const keywordName = req.params.keyword;
-    const url = `${ROOT_URL}&type=beer&withBreweries=Y&q=${keywordName}`;
+    const url = `${ROOT_URL}search?key=${keys.breweryKey}&type=beer&withBreweries=Y&q=${keywordName}`;
 
     axios
       .get(url)
       .then(response => {
         const results = response.data.data.slice(0, 12);
         res.send(results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+  app.get('/api/beer/:beerId', (req, res) => {
+    const beerId = req.params.beerId;
+    const url = `${ROOT_URL}beer/${beerId}?key=${keys.breweryKey}`;
+    console.log(url);
+
+    axios
+      .get(url)
+      .then(response => {
+        const result = response.data;
+        res.send(result);
       })
       .catch(error => {
         console.log(error);
