84cef115e0fa775771831661b8f845b8274909ae
Alexander Steele <alexsteele110@gmail.com>
2017-12-01 14:10:39 -0500

    added react-rating and now have a rating connected to redux review form

--- client/src/components/reviews/ReviewForm.js
+++ client/src/components/reviews/ReviewForm.js
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import Rating from 'react-rating';
+import StarBorderIcon from 'material-ui-icons/StarBorder';
+import StarIcon from 'material-ui-icons/Star';
 
-const ReviewForm = props => {
-  const { handleSubmit, pristine, reset, submitting } = props;
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Notes</label>
+class ReviewForm extends Component {
+  state = { starRating: 0 };
+
+  changeRate(name, value) {
+    this.props.change(name, value);
+    this.setState({ starRating: value });
+  }
+
+  render() {
+    const { handleSubmit, pristine, reset, submitting } = this.props;
+    return (
+      <form onSubmit={handleSubmit}>
+        <Field name="rating" type="hidden" component="input" />
+        <Rating
+          empty={<StarBorderIcon />}
+          full={<StarIcon />}
+          onClick={rate => {
+            this.changeRate('rating', rate);
+          }}
+          initialRate={this.state.starRating}
+        />
         <div>
-          <Field name="notes" component="textarea" />
+          <label>Notes</label>
+          <div>
+            <Field name="notes" component="textarea" />
+          </div>
         </div>
-      </div>
-      <div>
-        <button type="submit" disabled={pristine || submitting}>
-          Submit
-        </button>
-        <button type="button" disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </button>
-      </div>
-    </form>
-  );
-};
+
+        <div>
+          <button type="submit" disabled={pristine || submitting}>
+            Submit
+          </button>
+          <button
+            type="button"
+            disabled={pristine || submitting}
+            onClick={reset}
+          >
+            Clear Values
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
 
 export default reduxForm({
   form: 'review'
