5821cddf52595b094b5a510fd9a1ceeb94339c5a
Alexander Steele <alexsteele110@gmail.com>
2017-12-05 23:55:09 -0500

    refactored beerRoutes with async await

--- routes/beerRoutes.js
+++ routes/beerRoutes.js
@@ -4,53 +4,47 @@ const keys = require('../config/keys');
 const ROOT_URL = 'http://api.brewerydb.com/v2/';
 
 module.exports = app => {
-  app.get('/api/search/:keyword', (req, res) => {
+  app.get('/api/search/:keyword', async (req, res) => {
     const { keyword } = req.params;
     const url = `${ROOT_URL}search?key=${keys.breweryKey}&type=beer&withBreweries=Y&q=${keyword}`;
 
-    axios
-      .get(url)
-      .then(response => {
-        // API counts an empty search as a success. Checking for no data
-        // and then returning an empty array to represent no results
-        if (!response.data.data) {
-          res.send([]);
-        }
-        const results = response.data.data.slice(0, 12);
+    try {
+      const response = await axios.get(url);
+      const { data } = response.data;
+      // brewery API counts an empty search as a success. Checking for no data
+      // and then returning an empty array to represent no results
+      if (!data) {
+        res.send([]);
+      } else {
+        const results = data.slice(0, 12);
         res.send(results);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+      }
+    } catch (err) {
+      res.send(err);
+    }
   });
 
-  app.get('/api/beer/:beerId', (req, res) => {
+  app.get('/api/beer/:beerId', async (req, res) => {
     const { beerId } = req.params;
     const url = `${ROOT_URL}beer/${beerId}?key=${keys.breweryKey}&withBreweries=Y`;
 
-    axios
-      .get(url)
-      .then(response => {
-        const result = response.data;
-        res.send(result);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    try {
+      const response = await axios.get(url);
+      res.send(response.data);
+    } catch (err) {
+      res.send(err);
+    }
   });
 
-  app.get('/api/suggested/:styleId', (req, res) => {
+  app.get('/api/suggested/:styleId', async (req, res) => {
     const { styleId } = req.params;
     const url = `${ROOT_URL}beers/?key=${keys.breweryKey}&styleId=${styleId}&hasLabels=Y&withBreweries=Y&order=random&randomCount=3/`;
 
-    axios
-      .get(url)
-      .then(response => {
-        const results = response.data;
-        res.send(results);
-      })
-      .catch(error => {
-        console.log(error);
-      });
+    try {
+      const response = await axios.get(url);
+      res.send(response.data);
+    } catch (err) {
+      res.send(err);
+    }
   });
 };
