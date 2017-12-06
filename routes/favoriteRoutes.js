bb41d42fbc11c2148725c4f0cf8a774a94fc7984
Alexander Steele <alexsteele110@gmail.com>
2017-12-05 23:25:20 -0500

    refactored favoriteRoutes

--- routes/favoriteRoutes.js
+++ routes/favoriteRoutes.js
@@ -4,33 +4,21 @@ const requireLogin = require('../middlewares/requireLogin');
 const User = mongoose.model('users');
 
 module.exports = app => {
-  // Able to reduce repetitive code here?
   app.post('/api/favorites/:beerId', requireLogin, async (req, res) => {
     const { beerId } = req.params;
     const inFavorites = req.user.favoriteBeers.includes(beerId);
 
-    if (inFavorites) {
-      try {
-        const user = await User.findByIdAndUpdate(
-          req.user.id,
-          { $pull: { favoriteBeers: beerId } },
-          { new: true }
-        );
-        res.send(user);
-      } catch (err) {
-        res.send(error);
-      }
-    } else {
-      try {
-        const user = await User.findByIdAndUpdate(
-          req.user.id,
-          { $push: { favoriteBeers: beerId } },
-          { new: true }
-        );
-        res.send(user);
-      } catch (err) {
-        res.send(err);
-      }
+    try {
+      const user = await User.findByIdAndUpdate(
+        req.user.id,
+        inFavorites
+          ? { $pull: { favoriteBeers: beerId } }
+          : { $push: { favoriteBeers: beerId } },
+        { new: true }
+      );
+      res.send(user);
+    } catch (err) {
+      res.send(error);
     }
   });
 };
